eaa6e9da7f5eed08073b9bd97f5404f94bf76af7
Yilong Li <yilongl@cs.stanford.edu>
2016-10-21 20:32:16 -0700

    password-less ssh between nodes

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,2 +1,15 @@
+# Install RAMCloud dependencies
 apt-get update
 apt-get --assume-yes install build-essential git-core doxygen libpcre3-dev protobuf-compiler libprotobuf-dev libcrypto++-dev libevent-dev libboost-all-dev libgtest-dev libzookeeper-mt-dev zookeeper libssl-dev openjdk-8-jdk
+
+# Setup password-less ssh between nodes
+/usr/bin/geni-get key > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-keygen -y -f /root/.ssh/id_rsa > /root/.ssh/id_rsa.pub
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+chmod 644 /root/.ssh/authorized_keys
+cat >>/root/.ssh/config <<EOL
+Host *
+      StrictHostKeyChecking no
+EOL
+chmod 644 /root/.ssh/config
