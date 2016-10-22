4c121023f38865aba3051b2ac3ed8213f3228970
Yilong Li <yilongl@cs.stanford.edu>
2016-10-21 22:55:49 -0700

    password-less ssh for all users

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,3 +1,4 @@
+# What is the difference between `sudo bash basic_setup.sh` and appending `sudo` in front of commands in this script?
 # Install RAMCloud dependencies
 apt-get update
 apt-get --assume-yes install build-essential git-core doxygen libpcre3-dev protobuf-compiler libprotobuf-dev libcrypto++-dev libevent-dev libboost-all-dev libgtest-dev libzookeeper-mt-dev zookeeper libssl-dev openjdk-8-jdk
@@ -5,14 +6,25 @@ apt-get --assume-yes install build-essential git-core doxygen libpcre3-dev proto
 # Install other utilities
 apt-get --assume-yes vim pdsh
 
+# NFS
+apt-get --assume-yes nfs-kernel-server nfs-common
+
 # Setup password-less ssh between nodes
-/usr/bin/geni-get key > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-keygen -y -f /root/.ssh/id_rsa > /root/.ssh/id_rsa.pub
-cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-chmod 644 /root/.ssh/authorized_keys
-cat >>/root/.ssh/config <<EOL
-Host *
-      StrictHostKeyChecking no
+for user in `ls /users`; do
+    if [ "$user" = "geniuser" ]; then
+        continue
+    fi
+
+    ssh_dir=/users/$user/.ssh
+    /usr/bin/geni-get key > $ssh_dir/id_rsa
+    chmod 600 $ssh_dir/id_rsa
+    chown $user: $ssh_dir/id_rsa
+    ssh-keygen -y -f $ssh_dir/id_rsa > $ssh_dir/id_rsa.pub
+    cat $ssh_dir/id_rsa.pub >> $ssh_dir/authorized_keys
+    chmod 644 $ssh_dir/authorized_keys
+    cat >>$ssh_dir/config <<EOL
+    Host *
+         StrictHostKeyChecking no
 EOL
-chmod 644 /root/.ssh/config
+    chmod 644 $ssh_dir/config
+done
