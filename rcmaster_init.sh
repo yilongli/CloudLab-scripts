9c20f096621a45b159d7c3a78c39951081fd46c6
Yilong Li <yilongl@cs.stanford.edu>
2016-12-07 23:11:50 -0800

    fixed(?) nfs server service startup

--- rcmaster_init.sh
+++ rcmaster_init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo update-rc.d nfs-kernel-server enable
-# TODO: FIX THIS
+sudo /etc/init.d/nfs-kernel-server start
 pdsh -R ssh -w ^/shome/machines.txt \
     'sudo mkdir /shome; sudo mount rcmaster:/shome /shome;' \
     'echo "rcmaster:/shome /shome nfs rw,sync,hard,intr 0 0" | sudo tee -a /etc/fstab;' \
