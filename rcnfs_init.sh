7bc2a2039652cad932099a24165df1757872b8b3
Yilong Li <yilongl@cs.stanford.edu>
2016-12-15 21:29:38 -0800

    move rcnfs setup work before rebooting rcXX's

--- rcnfs_init.sh
+++ rcnfs_init.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Get RAMCloud
+cd /shome
+git clone https://github.com/PlatformLab/RAMCloud.git
+cd RAMCloud
+git submodule update --init --recursive
+ln -s ../../hooks/pre-commit .git/hooks/pre-commit
+cp /local/scripts/dpdkBuild.sh scripts/
+cp /local/scripts/cluster.py scripts/
+cp /local/scripts/common.py scripts/
+
+# Generate scripts/localconfig.py
+let num_rcXX=$(geni-get manifest | grep -o "<node " | wc -l)-2
+python /local/scripts/localconfigGen.py ${num_rcXX} > scripts/localconfig.py
+
 # NFS clients setup: use the publicly-routable IP addresses for both the server
 # and the clients to avoid interference with the experiment.
 rcnfs=$(hostname -i)
@@ -15,17 +29,3 @@ MLNX_OFED="MLNX_OFED_LINUX-3.4-1.0.0.0-ubuntu14.04-x86_64"
 pdsh -R ssh -w ^/shome/machines.txt -x rcnfs \
     "sudo /shome/$MLNX_OFED/mlnxofedinstall --force --without-fw-update;" \
     "sudo reboot"
-
-# Get RAMCloud
-cd /shome
-git clone https://github.com/PlatformLab/RAMCloud.git
-cd RAMCloud
-git submodule update --init --recursive
-ln -s ../../hooks/pre-commit .git/hooks/pre-commit
-cp /local/scripts/dpdkBuild.sh scripts/
-cp /local/scripts/cluster.py scripts/
-cp /local/scripts/common.py scripts/
-
-# Generate scripts/localconfig.py
-let num_rcXX=$(geni-get manifest | grep -o "<node " | wc -l)-2
-python /local/scripts/localconfigGen.py ${num_rcXX} > scripts/localconfig.py
